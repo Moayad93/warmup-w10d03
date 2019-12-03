fff6a9e87e9e2a9d72ff1149ecf3a58809d2e59a
Moayad Alnuwaysir <moayad.alnu@gmail.com>
2019-12-03 10:41:06 +0300

    Added my solution

--- Student.js
+++ Student.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
 
-
-const studentSchema = new Schema({
-    name: String,
+const studentSchema = new Schema(
+  {
+    name      : { type: String, required: true },
     Attendance: { type: Boolean, default: true }
+  },
+  { timestamps: true }
+);
 
-}, { timestamps: true });
-
-const Student = mongoose.model('Student', studentSchema);
+const Student = mongoose.model("Student", studentSchema);
 
 module.exports = Student;
